36af46b2885c55099321e7847c622d02750f955b
林传跑 <linchuanpao@wdai.com>
2018-05-20 14:51:18 +0800

    添加类

---
+++ src/main/java/FileTest.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C), 2011-2018, 微贷网.
+ */
+
+/**
+ * @author linchuanpao
+ * @time 2018/5/20  14:50
+ * @desc
+ */
+public class FileTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/MyTest.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C), 2011-2018, 微贷网.
+ */
+
+/**
+ * @author linchuanpao
+ * @time 2018/5/20  14:49
+ * @desc
+ */
+public class MyTest {
+    public static void main(String[] args) {
+
+    }
+}
